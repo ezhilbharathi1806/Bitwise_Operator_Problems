008e1cfc26439521927f5c43be8544151d6881f8
ezhilbharathi1806 <116344627+ezhilbharathi1806@users.noreply.github.com>
2025-01-08 21:59:05 +0530

    Update Odd_or_Even.c

--- Odd_or_Even.c
+++ Odd_or_Even.c
@@ -6,7 +6,7 @@ int main()
     printf("Enter a number: ");
     scanf("%d",&n);
     
-    if((n&1)==0){
+    if((n&1)==0){             // eg n=5 = 0101 & 0001 = 1 else part will get executed So odd
         printf("Even number: %d",n);
     }else{
         printf("Odd number: %d",n);
