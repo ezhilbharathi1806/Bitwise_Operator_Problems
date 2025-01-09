03f5c0f81d47231d83e678938f997670d9d952cb
ezhilbharathi1806 <116344627+ezhilbharathi1806@users.noreply.github.com>
2025-01-09 23:20:13 +0530

    Create 04_Subtract_Two_Numbers.c

---
+++ 04_Subtract_Two_Numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    printf("Enter two number: ");
+    scanf("%d%d",&a,&b);
+    
+    while(b!=0){
+        int data = (~a)&b;
+        a = a^b;
+        b = data<<1;
+    }
+    
+    printf("difference = %d",a);
+
+    return 0;
+}
